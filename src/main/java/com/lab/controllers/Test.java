7e244d38d2bab4476634b0d6512a07a2009a0691
alfonso.bravo <alfonso.bravo@es07110.esail.local>
2017-07-26 13:22:45 +0200

    Prueba 2

--- src/main/java/com/lab/controllers/Test.java
+++ src/main/java/com/lab/controllers/Test.java
@@ -1,7 +1,8 @@
 package com.lab.controllers;
 
 
-import com.lab.persistence.model.TestModel;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
@@ -9,27 +10,30 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.lab.persistence.model.TestModel;
 import com.lab.persistence.service.TestService;
-import java.util.List;
+
 
 
 @Controller
 public class Test
 {
-
-  @Autowired
-  TestService iService;
-
-
-  @ResponseBody
-  @RequestMapping(value = "/test",
-                  method = RequestMethod.POST,
-                  produces = MediaType.APPLICATION_JSON_VALUE)
-  public List<TestModel> test(@RequestBody TestModel obj) throws Exception
-  {
-    List<TestModel> x = iService.testService(obj);
-
-    return x;
-  }
-
+	
+	@Autowired
+	TestService iService;
+	
+	
+	
+	@ResponseBody
+	@RequestMapping(value = "/test",
+									method = RequestMethod.POST,
+									produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<TestModel> test(@RequestBody TestModel obj) throws Exception
+	{
+		List<TestModel> x = iService.testService(obj);
+		
+		return x;
+	}
+	
 }
